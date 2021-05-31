f9e3571e8c489b79d54650146ccfb9cd47e4226e
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2021-05-31 16:04:56 -0700

    Make baseline tests work

--- Test.fs
+++ Test.fs
@@ -1,13 +1,20 @@
 module Lex.Test
 open Lex
 let test name value =
+    let reference = "baselines/reference/" + name + ".baseline"
+    let local = "baselines/local/" + name + ".baseline"
     let actual = sprintf "%A" value
-    let expected = System.IO.File.ReadAllText("baselines/reference/" + name + ".baseline")
+    let expected = if System.IO.File.Exists reference then System.IO.File.ReadAllText reference else ""
     if actual <> expected then
-        System.IO.File.WriteAllText("baselines/local/" + name + ".baseline", actual)
+        System.IO.File.WriteAllText(local, actual)
         1
     else
         0
+let lexTests = [
+    "basicLex", "x"
+    "firstLex", " 1200Hello    World1! 14d"
+]
 let run () =
-    lex "x" |> test "basicLex" |> ignore
-    lex " 1200Hello    World1! 14d" |> test "firstLex"
+    lexTests 
+    |> List.map (fun (name,text) -> lex text |> test name) 
+    |> List.sum
